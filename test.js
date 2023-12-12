d54264cecfee9f1d6768def11597de77d84f1297
takahiro_aoki <136678255+takahiro-aoki816@users.noreply.github.com>
2023-12-13 02:38:31 +0900

    Create test.js

---
+++ test.js
@@ -0,0 +1,17 @@
+test.en = {
+  	gnav : function() {
+  		var tag = [];
+  		tag.push('<nav id="gnav">');
+  		tag.push('<ul class="cf nav-global content-inner">');
+  		tag.push('<li class="nav-global-e-top" data-gnav="top">' + this.localNaviTop() + '</li>');
+      tag.push('</ul>');
+		  tag.push('</nav>');
+    },
+  	localNaviTop : function() {
+  		var tag = [];
+  		tag.push('<h2 class="snav-top btn-toggle"><a href="/en/index.html">TOP</a></h2>');
+  		tag.push('<h3 class="view-pc"><a href="/en/index.html">TOP</a></h3>');
+  		return tag.join('');
+	  },
+};
+  
